f15fee61c7ba3fc6b01a8af3f8aa53575412d974
Truong Quoc Kiet <tqk03gl@gmail.com>
2024-09-10 09:52:15 +0700

    Update Frontend (p2)

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
   const Navbar = () => {
+
+    const [menu,setMenu] = useState("home");
+
     return (
       <div className='navbar'>
-        <img src={assets.ava_panda}/>
+        <img src={assets.ava_panda} className="logo"/>
         <ul className="navbar-menu">
-            <li>home</li>
-            <li>menu</li>
-            <li>contact</li>
-            <li>privacy</li>
+            <li onClick={() => setMenu("home")} className={menu==="home"?"active":""}>home</li>
+            <li onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+            <li onClick={() => setMenu("contact")} className={menu==="contact"?"active":""}>contact</li>
+            <li onClick={() => setMenu("privacy")} className={menu==="privacy"?"active":""}>privacy</li>
         </ul>
         <div className="navbar-right">
-            <li>search-icon</li>
+            <img/> {/* search icon */}
             <div className='navbar-search-icon'>
-                <img/>
+            <img/> {/*Shopping cart icon */}
                 <div className='dot'></div>
             </div>
+        
             <button>sign in</button>
         </div>
       </div>
